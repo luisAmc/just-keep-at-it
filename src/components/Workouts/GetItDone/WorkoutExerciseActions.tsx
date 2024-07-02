31a6ea4569906de6b68fa1d7688ece577d223e11
luisAmc <luis.kosm@gmail.com>
2024-07-01 21:24:35 -0600

    Change `WorkoutExerciseActions` from `Dropdown` to `Modal`

--- src/components/Workouts/GetItDone/WorkoutExerciseActions.tsx
+++ src/components/Workouts/GetItDone/WorkoutExerciseActions.tsx
@@ -8,63 +8,98 @@ import {
     TrashIcon,
 } from '@heroicons/react/24/outline';
 import { useWorkoutExercise } from './useWorkoutExercise';
-import {
-    Dropdown,
-    DropdownGroup,
-    DropdownItem,
-    DropdownLabel,
-} from '~/components/shared/Dropdown';
+import { Modal, useModal } from '~/components/shared/Modal';
+import { Button } from '~/components/shared/Button';
 
 export function WorkoutExerciseActions() {
+    const actionModal = useModal();
+
     const { isFirst, isLast, onChange, onMove, onRemove } =
         useWorkoutExercise();
 
+    function handleClick(action: () => void) {
+        actionModal.close();
+        action();
+    }
+
     return (
-        <Dropdown
-            className="mx-4"
-            trigger={<EllipsisVerticalIcon className="size-5" />}
-        >
-            <DropdownLabel>Cambio</DropdownLabel>
-            <DropdownGroup>
-                <DropdownItem onSelect={onChange}>
-                    <ArrowsRightLeftIcon className="mr-1 size-4" />
-                    <span>Cambiar ejercicio</span>
-                </DropdownItem>
-            </DropdownGroup>
+        <>
+            <Button size="icon" variant="ghost" onClick={actionModal.open}>
+                <EllipsisVerticalIcon className="size-5" />
+            </Button>
 
-            <DropdownLabel>Ubicación</DropdownLabel>
-            <DropdownGroup>
-                <DropdownItem
-                    disabled={isFirst}
-                    onSelect={() => onMove('first')}
-                >
-                    <ChevronDoubleUpIcon className="mr-1 size-4" />
-                    <span>Mover al incio</span>
-                </DropdownItem>
+            <Modal title="Acciones" {...actionModal.props}>
+                <div className="flex flex-col gap-y-1.5">
+                    <div className="mb-1 text-xs font-medium">Cambio</div>
 
-                <DropdownItem disabled={isFirst} onSelect={() => onMove('up')}>
-                    <ArrowUpIcon className="mr-1 size-4" />
-                    <span>Mover arriba</span>
-                </DropdownItem>
+                    <Button
+                        variant="secondary"
+                        className="justify-start bg-gray-100"
+                        onClick={() => handleClick(onChange)}
+                    >
+                        <ArrowsRightLeftIcon className="mr-1 size-4" />
+                        <span>Cambiar ejercicio</span>
+                    </Button>
+                </div>
 
-                <DropdownItem disabled={isLast} onSelect={() => onMove('down')}>
-                    <ArrowDownIcon className="mr-1 size-4" />
-                    <span>Mover abajo</span>
-                </DropdownItem>
+                <div className="mt-6 flex flex-col gap-y-1.5">
+                    <div className="mb-1 text-xs font-medium">Ubicación</div>
 
-                <DropdownItem disabled={isLast} onSelect={() => onMove('last')}>
-                    <ChevronDoubleDownIcon className="mr-1 size-4" />
-                    <span>Mover al final</span>
-                </DropdownItem>
-            </DropdownGroup>
+                    <Button
+                        disabled={isFirst}
+                        variant="secondary"
+                        className="justify-start bg-gray-100"
+                        onClick={() => handleClick(() => onMove('first'))}
+                    >
+                        <ChevronDoubleUpIcon className="mr-1 size-4" />
+                        <span>Mover al incio</span>
+                    </Button>
 
-            <DropdownLabel>Peligro</DropdownLabel>
-            <DropdownGroup>
-                <DropdownItem onSelect={onRemove} className="text-red-600">
-                    <TrashIcon className="mr-1 size-4" />
-                    <span>Remover ejercicio</span>
-                </DropdownItem>
-            </DropdownGroup>
-        </Dropdown>
+                    <Button
+                        disabled={isFirst}
+                        variant="secondary"
+                        className="justify-start bg-gray-100"
+                        onClick={() => handleClick(() => onMove('up'))}
+                    >
+                        <ArrowUpIcon className="mr-1 size-4" />
+                        <span>Mover arriba</span>
+                    </Button>
+
+                    <Button
+                        disabled={isLast}
+                        variant="secondary"
+                        className="justify-start bg-gray-100"
+                        onClick={() => handleClick(() => onMove('down'))}
+                    >
+                        <ArrowDownIcon className="mr-1 size-4" />
+                        <span>Mover abajo</span>
+                    </Button>
+
+                    <Button
+                        disabled={isLast}
+                        variant="secondary"
+                        className="justify-start bg-gray-100"
+                        onClick={() => handleClick(() => onMove('last'))}
+                    >
+                        <ChevronDoubleDownIcon className="mr-1 size-4" />
+                        <span>Mover al final</span>
+                    </Button>
+                </div>
+
+                <div className="mt-6 flex flex-col gap-y-1.5">
+                    <div className="mb-1 text-xs font-medium text-red-700">
+                        Peligro
+                    </div>
+
+                    <Button
+                        variant="destructive"
+                        onClick={() => handleClick(onRemove)}
+                    >
+                        <TrashIcon className="mr-1 size-4" />
+                        <span>Remover ejercicio</span>
+                    </Button>
+                </div>
+            </Modal>
+        </>
     );
 }
