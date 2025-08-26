b2a22e76b8dce1838fabdeb7ef302b9ff6565b81
luisAmc <luis.kosm@gmail.com>
2025-08-25 23:05:34 -0600

    Removes textarea border

--- src/components/shared/SimpleInput.tsx
+++ src/components/shared/SimpleInput.tsx
@@ -2,8 +2,8 @@ import { cva, VariantProps } from 'class-variance-authority';
 import { ComponentPropsWithRef, forwardRef } from 'react';
 
 export const simpleTextareaVariants = cva([
-    'field-sizing-content resize-none px-3 py-2 text-[16px] md:text-sm w-full rounded-lg border-b border-solid border-brand-200 text-brand-800 placeholder:text-brand-600',
-    'focus:border-brand-200 focus:outline-none',
+    'field-sizing-content resize-none px-3 py-2 text-[16px] md:text-sm w-full rounded-lg text-brand-800 placeholder:text-brand-600',
+    'focus:outline-none',
     'disabled:opacity-60 disabled:pointer-events-none',
     'appearance-none transition ease-in-out',
 ]);
